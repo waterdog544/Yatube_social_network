20a6d9d326a478475de3327c4993997ec425a5b6
Андрей Шванов <shvanov@mail.ru>
2022-03-29 09:44:50 +0300

    sprint 6

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -2,7 +2,6 @@ from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, redirect, render
-# from django.views.decorators.cache import cache_page
 from posts.forms import CommentForm, PostForm
 from posts.models import Follow, Group, Post, User
 
